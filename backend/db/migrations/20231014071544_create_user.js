482903a40db50e21696be58987284a29ff8ee376
gautam-bhagat <gautambhagat.business@gmail.com>
2023-10-14 13:58:24 +0530

    signup-endpoint complete

--- backend/db/migrations/20231014071544_create_user.js
+++ backend/db/migrations/20231014071544_create_user.js
@@ -1,9 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", (table) => {
     table.increments("id");
-    table.string("username");
+    table.string("name");
     table.string("email");
     table.string("password");
+    table.string("token");
     table.timestamps(true, true);
   });
 };
